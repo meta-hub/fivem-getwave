6520dcc67a83c6e44b6c2a782246d8b216b99e12
meta-hub <56391287+meta-hub@users.noreply.github.com>
2020-08-25 13:31:57 +0930

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -52,9 +52,12 @@ RegisterCommand('wave:visualize', function(source,args)
       local target      = math.max(1,math.min(#wave,math.floor(since * durations)))
       local wave_target = wave[target]
 
-      local p1 = pos + (fwd*5.0) + vector3(0.0,0.0, wave_target)
-
-      DrawBox(p1.x-0.5,p1.y-0.5,p1.z-0.5, p1.x+0.5,p1.y+0.5,p1.z+0.5, math.floor(255*(wave_target+0.5)),150-math.floor(255*wave_target),255-math.floor(255*(wave_target-0.5)),200)
+      local mod = 0.5
+      local modifier = math.min(5.0,math.max(-5.0,wave_target*5.0))
+      for i=-2.4,2.4,0.1 do
+        local p1 = PointOnSphere(5.0 * (i*15.0),0.0,(5.0 + (5.0 * (modifier*i)))) + pos + (GetEntityForwardVector(ped) * 10.0) + vector3(0.0,0.0,wave_target * math.abs(i))
+        DrawBox(p1.x-mod,p1.y-mod,p1.z-(mod*modifier), p1.x+mod,p1.y+mod,p1.z+(mod*modifier), math.floor(255*(wave_target)),150-math.floor(255*wave_target),255-math.floor(255*(wave_target)),200)
+      end
 
       if since >= duration then
         return
@@ -63,3 +66,23 @@ RegisterCommand('wave:visualize', function(source,args)
     end
   end)
 end)
+
+
+GetVecDist = function(v1,v2)
+  if not v1 or not v2 or not v1.x or not v2.x then return 0; end
+  return math.sqrt(  ( (v1.x or 0) - (v2.x or 0) )*(  (v1.x or 0) - (v2.x or 0) )+( (v1.y or 0) - (v2.y or 0) )*( (v1.y or 0) - (v2.y or 0) )+( (v1.z or 0) - (v2.z or 0) )*( (v1.z or 0) - (v2.z or 0) )  )
+end
+
+PointOnSphere = function(alt,azu,radius,orgX,orgY,orgZ)
+  local toradians = 0.017453292384744
+  alt,azu,radius,orgX,orgY,orgZ = ( tonumber(alt or 0) or 0 ) * toradians, ( tonumber(azu or 0) or 0 ) * toradians, tonumber(radius or 0) or 0, tonumber(orgX or 0) or 0, tonumber(orgY or 0) or 0, tonumber(orgZ or 0) or 0
+  if      vector3
+  then
+      return
+      vector3(
+           orgX + radius * math.sin( azu ) * math.cos( alt ),
+           orgY + radius * math.cos( azu ) * math.cos( alt ),
+           orgZ + radius * math.sin( alt )
+      )
+  end
+end
